61da10b2407ab13de48fef8df681ba804173bb0e
Herbew <herbew@gmail.com>
2022-04-27 14:37:48 +0700

    ten

--- test/romanumber_test.exs
+++ test/romanumber_test.exs
@@ -1,9 +1,14 @@
 defmodule RomanNumberConverterTest do
     use ExUnit.Case
-	# @tag :pending
+	@tag :pending
 	test "1. Numeral(1)" do
 		assert RomanNumberConverter.numeral(1) == "I"
-	end #test "1" do
+	end #test "1. Numeral(1)" do
+	
+	@tag :pending
+	test "2. Numeral(10)" do
+		assert RomanNumberConverter.numeral(10) == "X"
+	end #test "2. Numeral(10)" do
   
     
 end
